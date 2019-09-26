75838170294db25c21d07bb5438772530dc2a0d1
paulon <paulon@playstudios.com>
2019-09-25 22:03:19 -0700

    added axios dependency + started working on Standings

--- src/js/NavBar.js
+++ src/js/NavBar.js
@@ -10,7 +10,8 @@ import {
 import Home from "../js/Home";
 import LmNotes from "../js/LmNotes";
 import Managers from "../js/Managers";
-import ManagerPage from "./ManagerPage";
+import ManagerPage from "../js/ManagerPage";
+import Standings from "../js/Standings";
 
 class NavBar extends Component {
     render() {
@@ -22,12 +23,14 @@ class NavBar extends Component {
                         <NavLink exact to="/">Home</NavLink>
                         <NavLink to="/lm-notes">LM Notes</NavLink>
                         <NavLink to="/managers">Managers</NavLink>
+                        <NavLink to="/standings">Standings</NavLink>
                     </nav>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
                         <Route path="/lm-notes" component={LmNotes}/>
                         <Route exact path="/managers" component={Managers}/>
                         <Route exact path="/managers/:name" component={ManagerPage}/>
+                        <Route path="/standings" component={Standings}/>
                     </div>
                 </div>
             </HashRouter>
---
+++ src/js/Standings.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+import Axios from "axios";
+
+class Standings extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+                <h1> Current FTP Standings </h1>
+            </div>
+        );
+    }
+}
+
+export default Standings;
