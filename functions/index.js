75ad117db2c7b0b0300c231ecc6f15da8b9e2711
pnasciment0 <thepaulonascimento@gmail.com>
2019-09-26 20:24:37 -0700

    update 9/26

--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,51 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const request = require('request');
 const cheerio = require('cheerio');
+
+require('dotenv').config();
+
 let rp = require('request-promise');
 
 const app = express();
 
+const GLOBAL_LEAGUE_ID = process.env.LEAGUE_ID;
+
 app.use(cors({
     origin: true
 }));
+app.use(bodyParser.json());
 
-app.get("/test", (req, res) => {
-   res.send("hi");
+app.get("/standings", (req, res) => {
+   sendStandingsData();
 });
 
-const api = functions.https.onRequest(app);
+async function getStandingsData() {
+    let url = 'https://fantasy.espn.com/football/league/standings?leagueId=${GLOBAL_LEAGUE_ID}';
+    try {
+        const response = await rp(url);
+        let $ = cheerio.load(response);
+        return Promise.resolve("test");
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
 
+async function sendStandingsData() {
+    const res = await getStandingsData();
+}
+
+let url = `https://fantasy.espn.com/football/league/standings?leagueId=${GLOBAL_LEAGUE_ID}`;
+const response = rp(url);
+let $ = cheerio.load(response);
+let items = $("span.teamName");
+console.log(items);
+for (let i = 0; i < items.length; i++) {
+    console.log("test");
+    console.log($(items[i]));
+}
+
+
+const api = functions.https.onRequest(app);
 module.exports = {
     api
 };
--- src/js/Standings.js
+++ src/js/Standings.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 
-import Axios from "axios";
+import axios from "axios";
 
 class Standings extends Component {
-    componentDidMount() {
+    state = {
+        response: []
+    }
 
+    componentDidMount() {
+        axios.get("https://us-central1-ftpfantasy-f75a6.cloudfunctions.net/api/test")
+            .then(res => {
+                const response = res.data;
+                this.setState({response});
+            })
     }
 
     render() {
         return(
             <div>
                 <h1> Current FTP Standings </h1>
+                <h2> Response from server:</h2>
+                <p>{this.state.response}</p>
             </div>
         );
     }
